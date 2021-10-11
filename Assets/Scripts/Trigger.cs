49129c0c1990b037308ed42f64c419adb0284043
Andreas <adde.engberg@gmail.com>
2021-10-11 20:56:38 +0200

    Fixed easy player kill setup on trigger

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -10,19 +10,38 @@ public class Trigger : MonoBehaviour
 {
     public RGBChannel gunColor;
 
+    public bool KillPlayer = false;
+    public bool fireEventOnce = false;
+
     public UnityEvent onTriggered;
 
     public UnityEvent<Vector3> onTriggeredVector;
 
     public UnityEvent<RGBChannel> onTriggerChannel;
 
+    private bool eventFired = false;
+    private void Start()
+    {
+        if (KillPlayer)
+        {
+            onTriggered.AddListener(Player.Instance.Kill);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(fireEventOnce)
+        {
+            if (eventFired)
+                return;
+        }
+
         if (other.name == "Player")
         {
             onTriggered.Invoke();
             onTriggeredVector.Invoke(transform.position);
             onTriggerChannel.Invoke(gunColor);
+            eventFired = true;
         }
     }
 
