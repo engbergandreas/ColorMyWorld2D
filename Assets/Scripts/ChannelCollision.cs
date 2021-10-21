0e27ec1799609752a4bd8a461e0fd85acc0119df
Andreas <adde.engberg@gmail.com>
2021-10-21 08:47:57 +0200

    Added red,blue,green sprites for blocking walls / pass through

--- Assets/Scripts/ChannelCollision.cs
+++ Assets/Scripts/ChannelCollision.cs
@@ -14,10 +14,60 @@ public class ChannelCollision : MonoBehaviour
 
     private BoxCollider2D col;
 
+    public SpriteRenderer sr;
+    public Sprite fullColor, blue_red, blue_green, red_green;
+
     private void Start()
     {
         col = GetComponent<BoxCollider2D>();
         ColorGun.Instance.rgbChannelEvent.AddListener(OnChannelChange);
+        GetCorrectSprite();
+    }
+
+    private void GetCorrectSprite()
+    {
+        Sprite sprite = fullColor;
+        switch (canWalkThroughInChannel)
+        {
+            case RGBChannel.Red:
+                if (solidInChannel)
+                {
+                    sprite = fullColor;
+                    sr.color = new Color(1, 0, 0, 0.5f);
+                }
+                else
+                {
+                    sprite = blue_green;
+                    sr.color = new Color(1, 1, 1, 0.5f);
+                }
+                break;
+            case RGBChannel.Green:
+                if (solidInChannel) {
+                    sprite = fullColor;
+                    sr.color = new Color(0, 1, 0, 0.5f);
+                }
+                else
+                {
+                    sprite = blue_red;
+                    sr.color = new Color(1, 1, 1, 0.5f);
+                }
+                break;
+            case RGBChannel.Blue:
+                if (solidInChannel)
+                {
+                    sprite = fullColor;
+                    sr.color = new Color(0, 0, 1, 0.5f);
+                }
+                else
+                {
+                    sprite = red_green;
+                    sr.color = new Color(1, 1, 1, 0.5f);
+                }
+                break;
+            default:
+                break;
+        }
+        sr.sprite = sprite;
     }
 
     private void OnChannelChange(RGBChannel newChannel)
