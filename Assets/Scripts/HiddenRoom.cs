17d6a4a015a118b5707c5d4752dba37af046393b
Andreas <adde.engberg@gmail.com>
2021-10-21 13:21:58 +0200

    Added tilemap over hidden room

--- Assets/Scripts/HiddenRoom.cs
+++ Assets/Scripts/HiddenRoom.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class HiddenRoom : MonoBehaviour
 {
     public GameObject room;
+    public TilemapRenderer tmap;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if (collision.transform.tag == "Player")
+        {
+            tmap.enabled = false;
             room.SetActive(true);
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if (collision.transform.tag == "Player")
+        {
+            tmap.enabled = true;
             room.SetActive(false);
+        }
     }
 }
